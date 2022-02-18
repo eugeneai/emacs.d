67d17e7986067a8d64c40f0835b4af37aaf6d2f4
Evgeny Cherkashin <eugeneai@irnok.net>
2022-02-18 12:32:41 +0800

    Turn off cool Ctrl-s swiper too.

--- init.el
+++ init.el
@@ -1143,7 +1143,7 @@
   :config
   (ivy-mode 1)
   (setq ivy-use-virtual-buffers t)
-  (global-set-key "\C-s" 'swiper)
+  ;; (global-set-key "\C-s" 'swiper)
   (global-set-key (kbd "C-c C-r") 'ivy-resume)
   (global-set-key (kbd "<f6>") 'ivy-resume)
   ;; (global-set-key (kbd "M-x") 'counsel-M-x)
